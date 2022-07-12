9440d25e507a8cba66a676359324a1f8e7ed59ce
sam <sam@DESKTOP-7GFKDTB.localdomain>
2022-07-12 20:43:20 +0200

    minor fixes

--- thepage/src/App.tsx
+++ thepage/src/App.tsx
@@ -14,10 +14,10 @@ function App() {
     <BrowserRouter>
       <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Videos" element={<Videos />} />
-            <Route path="/Blogs" element={<Blogs />} />
+            <Route path="/thepage" element={<Home />} />
+            <Route path="/thepage/Home" element={<Home />} />
+            <Route path="/thepage/Videos" element={<Videos />} />
+            <Route path="/thepage/Blogs" element={<Blogs />} />
         </Routes >
   </BrowserRouter>
   </div>
